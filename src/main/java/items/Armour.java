29950a116757c7915d9eb52749d19498fda67d6d
garyb6 <gary.bennett6@outlook.com>
2022-02-18 17:03:12 +0000

    armour constructor built and element enum incorporated

--- src/main/java/items/Armour.java
+++ src/main/java/items/Armour.java
@@ -1,4 +1,18 @@
 package items;
 
-public abstract class Armour {
+import items.ENUMs.ItemType;
+
+import java.lang.annotation.ElementType;
+
+public abstract class Armour extends Items {
+
+    private int defence;
+    private ElementType element;
+    private int bodySlot;
+    protected Armour(String name, String description, ItemType type, int stockPrice, int sellPrice, int defence, ElementType element, int bodySlot) {
+        super(name, description, type, stockPrice, sellPrice);
+        this.defence = defence;
+        this.element = element;
+        this.bodySlot = bodySlot;
+    }
 }
