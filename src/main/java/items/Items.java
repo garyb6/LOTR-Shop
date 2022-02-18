591c86d45be597abd71e5f31f1be28b031f5266f
garyb6 <gary.bennett6@outlook.com>
2022-02-18 16:57:34 +0000

    items constructor built, enum incorporated

--- src/main/java/items/Items.java
+++ src/main/java/items/Items.java
@@ -1,17 +1,19 @@
 package items;
 
+import items.ENUMs.ItemType;
+
 public abstract class Items {
 
     private String name;
     private String description;
-    private Enum Type;
+    private ItemType type;
     private int stockPrice;
     private int sellPrice;
 
-    protected Items(String name, String description, Enum type, int stockPrice, int sellPrice) {
+    protected Items(String name, String description, ItemType type, int stockPrice, int sellPrice) {
         this.name = name;
         this.description = description;
-        Type = type;
+        this.type = type;
         this.stockPrice = stockPrice;
         this.sellPrice = sellPrice;
     }
