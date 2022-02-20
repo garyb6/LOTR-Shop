ecc7a8c22627966d1b6870b23b4dab549a3fa1e4
garyb6 <gary.bennett6@outlook.com>
2022-02-20 12:44:31 +0000

    added shop class

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -1,4 +1,32 @@
 package shop;
 
+import behaviours.ISell;
+
+import java.util.ArrayList;
+
 public class Shop{
+
+    private String name;
+    private ArrayList<ISell> stock;
+
+    public Shop(String name, ArrayList<ISell> stock) {
+        this.name = name;
+        this.stock = stock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<ISell> getStock() {
+        return stock;
+    }
+
+    public void setStock(ArrayList<ISell> stock) {
+        this.stock = stock;
+    }
 }
